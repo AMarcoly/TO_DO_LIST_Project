2cd5e7195a5473cd979cd6774743a558f30d6406
fellah <abdoulazizdiallofouta@gmail.com>
2024-01-07 22:22:38 +0100

    Comment those index

--- index.sql
+++ index.sql
@@ -11,6 +11,6 @@ CREATE INDEX idx_depend_de_ref_tache_1 ON Depend_de(ref_tache_1);
 CREATE INDEX idx_depend_de_ref_tache_2 ON Depend_de(ref_tache_2);
 
 -- Pour optimiser la 5ème requête.
-CREATE INDEX idx_tache_fini_date_realisation ON Tache_fini(date_realisation);
-CREATE INDEX idx_score_cat_tache_nom_cat ON Score_categorie_tache(nom_categorie);
+-- CREATE INDEX idx_tache_fini_date_realisation ON Tache_fini(date_realisation);
+-- CREATE INDEX idx_score_cat_tache_nom_cat ON Score_categorie_tache(nom_categorie);
 CREATE INDEX idx_est_assigne_ref_utilisateur ON Est_assigne(ref_utilisateur);
